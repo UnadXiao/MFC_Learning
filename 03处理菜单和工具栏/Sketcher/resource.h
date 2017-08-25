cc43afd5a413aa2b87385d5b020086d804efa4fd
MarkXiao <xiaozp@daheng-image.com>
2017-08-25 18:54:18 +0000

    上传文件至 '03处理菜单和工具栏/Sketcher'

---
+++ 03处理菜单和工具栏/Sketcher/resource.h
@@ -0,0 +1,32 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by Sketcher.rc
+//
+#define IDD_ABOUTBOX                    100
+#define IDR_MAINFRAME                   128
+#define IDR_SketcherTYPE                130
+#define ID_WINDOW_MANAGER               131
+#define ID_ELEMENT_LINE                 32771
+#define ID_ELEMENT_LINE32772            32772
+#define ID_ELEMENT_LINE32773            32773
+#define ID_ELEMENT_LINE32774            32774
+#define ID_ELEMET_LINE                  32775
+#define ID_ELEMET_RECTANGLE             32776
+#define ID_ELEMET_CIRCLE                32777
+#define ID_ELEMET_CURVE                 32778
+#define ID_COLOR_BLACK                  32779
+#define ID_COLOR_RED                    32780
+#define ID_COLOR_GREEN                  32781
+#define ID_COLOR_BLUE                   32782
+#define ID_BUTTON32794                  32794
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        310
+#define _APS_NEXT_COMMAND_VALUE         32798
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           310
+#endif
+#endif
