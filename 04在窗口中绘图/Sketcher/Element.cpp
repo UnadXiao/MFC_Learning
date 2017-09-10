0cef1d6ce53c825b858bfdee59ab15832f77826b
MarkXiao <xiaozp@daheng-image.com>
2017-09-10 15:11:18 +0000

    上传文件至 '04在窗口中绘图/Sketcher'

---
+++ 04在窗口中绘图/Sketcher/Element.cpp
@@ -0,0 +1,18 @@
+#include "stdafx.h"
+#include "Sketcher.h"
+#include "Element.h"
+
+
+
+CElement::CElement()
+{
+	m_PenWidth = 0;
+}
+
+CElement::CElement(const CPoint start, COLORREF color, int penWidth) :
+	m_StartPoint{start}, m_Color(color), m_PenWidth(penWidth) {}
+
+CElement::~CElement()
+{
+
+}
---
+++ 04在窗口中绘图/Sketcher/Line.cpp
@@ -0,0 +1,36 @@
+#include "stdafx.h"
+#include "Line.h"
+
+
+CLine::CLine()
+{
+}
+
+
+CLine::~CLine()
+{
+}
+
+void CLine::Draw(CDC * pDC)
+{
+	// Create a pen for this object and initialize it
+	CPen aPen;
+	CreatePen(aPen);
+
+	CPen* pOldPen{ pDC->SelectObject(&aPen) };		// Select the pen
+
+	// Now draw the line
+	pDC->MoveTo(m_StartPoint);
+	pDC->LineTo(m_EndPoint);
+	
+	pDC->SelectObject(pOldPen);		// Restore the old pen
+}
+
+CLine::CLine(const CPoint & start, const CPoint & end, COLORREF color) :
+	CElement{start, color}, m_EndPoint(end)
+{
+	// Define the enclosing rectangle
+	m_EnclosingRect = CRect{ start, end };
+	m_EnclosingRect.NormalizeRect();
+	m_EnclosingRect.InflateRect(m_PenWidth, m_PenWidth);
+}
